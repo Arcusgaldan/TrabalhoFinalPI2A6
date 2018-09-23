c862039a70ba0f3259e66f54d38a494a2d6fd5dd
Gabiruu <gaalvesreis@gmail.com>
2018-09-23 19:04:56 -0300

    Listando Usuarios em cadastroUsuario

--- frontEndTrabalho/js/listarUsuario.js
+++ frontEndTrabalho/js/listarUsuario.js
@@ -1,43 +1,62 @@
 function criaElementos(listaUsuario){
+	console.log("Tamanho vetor" + listaUsuario.length);
 	for(var i=0;i<listaUsuario.length;i++){
-		$("#tabelaUsuario").append('<tr>\
-		    <th id="nomeUsuarioLista'+ i +'"></th>\
+		$("#tabelaUsuario").append("\
+		<tr>\
+		    <th id='nomeUsuarioLista"+ i +"'></th>\
 		    <td>\
-		      <button class="btn btn-info" scope="row" data-toggle="collapse" href="#collapseUsuarioLista'+ i +'" role="button" aria-expanded="false" aria-controls="collapseExample"> Mostra Dados <span class="fas fa-plus"></span></button>\
-			  <button id="alterarUsuarioLista'+ i +'" class="btn btn-warning" data-toggle="modal" data-target="#alteraModal" >Alterar Usuário</button>\
-		      <button id="excluirUsuarioLista'+ i +'" class="btn btn-danger" data-toggle="modal" data-target="#excluirModal">Excluir Usuário</button>\
+				<button class='btn btn-info' scope='row' data-toggle='collapse' href='#collapseUsuarioLista"+ i +"' role='button' aria-expanded='false' aria-controls='collapseExample'> Mostra Dados <span class='fas fa-plus'></span></button>\
+				<button id='alterarUsuarioLista"+ i +"' class='btn btn-warning' data-toggle='modal' data-target='#alteraModal' >Alterar Usuário</button>\
+				<button id='excluirUsuarioLista"+ i +"' class='btn btn-danger' data-toggle='modal' data-target='#excluirModal'>Excluir Usuário</button>\
+				<div id='collapseUsuarioLista"+ i +"' class='collapse mostraLista' >\
+				  <div class='card card-body'>\
+				    <p><strong>Nome: </strong><span id='nomeUsuarioDados"+i+"'></span></p>\
+				    <p><strong>Prontuário: </strong> <span id='prontuarioUsuarioDados"+i+"'></span></p>\
+				    <p><strong>E-Mail: </strong> <span id='emailUsuarioDados"+i+"'></span></p>\
+				    <p><strong>Link do Lattes: </strong> <span id='lattesUsuarioDados"+i+"'></span></p>\
+				    <p><strong>Tipo de Usuário: </strong> <span id='tipoUsuarioDados"+i+"'></span></p>\
+				  </div>\
+				</div>\
 		    </td>\
-		    <div id="collapseUsuarioLista'+ i +'" class="collapse" > \
-		      <div class="card card-body">\
-		        <p><strong>Nome: </strong><span id="nomeUsuarioDados'+i+'"></span></p>\
-		        <p><strong>Prontuário: </strong> <span id="prontuariUsuarioDados'+i+'"></span></p>\
-		        <p><strong>E-Mail: </strong> <span id="emailUsuarioDados'+i+'"></span></p>\
-		        <p><strong>Link do Lattes: </strong> <span id="lattesUsuarioDados'+i+'"></span></p>\
-		        <p><strong>Tipo de Usuário: </strong> <span id="tipoUsuarioDados'+i+'"></span></p>\
-		      </div>\
-		    </div>\
 		  </tr>\
-		');
+		");
+		//Puxa o nome que refenrencia o usuário na row
+		document.getElementById("nomeUsuarioLista" + i).innerHTML = listaUsuario[i].nome;
 
-		document.getElementById("alterarUsuarioLista"+ i).addEventListener("click", function(){preencheModalAlterar(listaUsuario[i])});
+		//Puxa os dados para o collapse de exibição deusuário
+		document.getElementById("nomeUsuarioDados" + i).innerHTML = listaUsuario[i].nome;
+		document.getElementById("prontuarioUsuarioDados" + i).innerHTML = listaUsuario[i].prontuario;
+		document.getElementById("emailUsuarioDados" + i).innerHTML = listaUsuario[i].email;
+		document.getElementById("lattesUsuarioDados" + i).innerHTML = listaUsuario[i].curriculoLattes;
+		if(listaUsuario[i].codTipoUsuario == 1){	
+			document.getElementById("tipoUsuarioDados" + i).innerHTML = "Lider";
 
-		document.getElementById("excluirUsuarioLista"+ i).addEventListener("click", function(){preencheModalExcluir(listaUsuario[i])});
+		}else{
+			document.getElementById("tipoUsuarioDados" + i).innerHTML = "Administrador";
+		}
+
+		var funcaoPreencheAlterar = preencheModalAlterar(listaUsuario[i]);
+
+		document.getElementById("alterarUsuarioLista"+ i).addEventListener("click", funcaoPreencheAlterar);
+
+		document.getElementById("excluirUsuarioLista"+ i).addEventListener("click", function(){
+			preencheModalExcluir(listaUsuario[i]);
+		});
 
 	}
+}
 
-	function preencheModalAlterar(usuario){
-		document.getElementById("nomeAlterar").value = usuario.nome;
-		document.getElementById("prontuarioAlterar").value = usuario.prontuario;
-		document.getElementById("emailAlterar").value = usuario.email;
-		document.getElementById("linkLattesAlterar").value = usuario.lattes;
-		document.getElementById("fotoAlterar").value = usuario.foto;
-	}
+function preencheModalAlterar(usuario){
+	console.log("entrei no modal Alterar com usuario = " + JSON.stringify(usuario));
+	document.getElementById("nomeAlterar").value = usuario.nome;
+	document.getElementById("prontuarioAlterar").value = usuario.prontuario;
+	document.getElementById("emailAlterar").value = usuario.email;
+	document.getElementById("linkLattesAlterar").value = usuario.lattes;
+	document.getElementById("fotoAlterar").value = usuario.foto;
+}
 
-	function preencheModalExcluir(usuario){
-		document.getElementById("nomeExcluir").value = usuario.nome;
-	}
-
-
+function preencheModalExcluir(usuario){
+	document.getElementById("nomeExcluir").innerHTML = "Você Realmente deseja excluir" + usuario.nome + "?";
 }
 
 var utils = require('./../../utils.js');
@@ -50,8 +69,7 @@ var req = http.request(opcoesHTTP, (res) => {
 	res.on('data', function(chunk){
 		console.log("Chunk: " + chunk);
 		let vetor = JSON.parse(chunk);
-		
-		document.getElementById('nomeUsuaroioLista0').innerHTML = vetor[0].nome;
+		criaElementos(vetor);
 	});
 });
 req.end();
--- frontEndTrabalho/js/listarUsuarioFront.js
+++ frontEndTrabalho/js/listarUsuarioFront.js
@@ -24685,6 +24685,67 @@ function extend() {
 }
 
 },{}],167:[function(require,module,exports){
+function criaElementos(listaUsuario){
+	console.log("Tamanho vetor" + listaUsuario.length);
+	for(var i=0;i<listaUsuario.length;i++){
+		$("#tabelaUsuario").append("\
+		<tr>\
+		    <th id='nomeUsuarioLista"+ i +"'></th>\
+		    <td>\
+				<button class='btn btn-info' scope='row' data-toggle='collapse' href='#collapseUsuarioLista"+ i +"' role='button' aria-expanded='false' aria-controls='collapseExample'> Mostra Dados <span class='fas fa-plus'></span></button>\
+				<button id='alterarUsuarioLista"+ i +"' class='btn btn-warning' data-toggle='modal' data-target='#alteraModal' >Alterar Usuário</button>\
+				<button id='excluirUsuarioLista"+ i +"' class='btn btn-danger' data-toggle='modal' data-target='#excluirModal'>Excluir Usuário</button>\
+				<div id='collapseUsuarioLista"+ i +"' class='collapse mostraLista' >\
+				  <div class='card card-body'>\
+				    <p><strong>Nome: </strong><span id='nomeUsuarioDados"+i+"'></span></p>\
+				    <p><strong>Prontuário: </strong> <span id='prontuarioUsuarioDados"+i+"'></span></p>\
+				    <p><strong>E-Mail: </strong> <span id='emailUsuarioDados"+i+"'></span></p>\
+				    <p><strong>Link do Lattes: </strong> <span id='lattesUsuarioDados"+i+"'></span></p>\
+				    <p><strong>Tipo de Usuário: </strong> <span id='tipoUsuarioDados"+i+"'></span></p>\
+				  </div>\
+				</div>\
+		    </td>\
+		  </tr>\
+		");
+		//Puxa o nome que refenrencia o usuário na row
+		document.getElementById("nomeUsuarioLista" + i).innerHTML = listaUsuario[i].nome;
+
+		//Puxa os dados para o collapse de exibição deusuário
+		document.getElementById("nomeUsuarioDados" + i).innerHTML = listaUsuario[i].nome;
+		document.getElementById("prontuarioUsuarioDados" + i).innerHTML = listaUsuario[i].prontuario;
+		document.getElementById("emailUsuarioDados" + i).innerHTML = listaUsuario[i].email;
+		document.getElementById("lattesUsuarioDados" + i).innerHTML = listaUsuario[i].curriculoLattes;
+		if(listaUsuario[i].codTipoUsuario == 1){	
+			document.getElementById("tipoUsuarioDados" + i).innerHTML = "Lider";
+
+		}else{
+			document.getElementById("tipoUsuarioDados" + i).innerHTML = "Administrador";
+		}
+
+		var funcaoPreencheAlterar = preencheModalAlterar(listaUsuario[i]);
+
+		document.getElementById("alterarUsuarioLista"+ i).addEventListener("click", funcaoPreencheAlterar);
+
+		document.getElementById("excluirUsuarioLista"+ i).addEventListener("click", function(){
+			preencheModalExcluir(listaUsuario[i]);
+		});
+
+	}
+}
+
+function preencheModalAlterar(usuario){
+	console.log("entrei no modal Alterar com usuario = " + JSON.stringify(usuario));
+	document.getElementById("nomeAlterar").value = usuario.nome;
+	document.getElementById("prontuarioAlterar").value = usuario.prontuario;
+	document.getElementById("emailAlterar").value = usuario.email;
+	document.getElementById("linkLattesAlterar").value = usuario.lattes;
+	document.getElementById("fotoAlterar").value = usuario.foto;
+}
+
+function preencheModalExcluir(usuario){
+	document.getElementById("nomeExcluir").innerHTML = "Você Realmente deseja excluir" + usuario.nome + "?";
+}
+
 var utils = require('./../../utils.js');
 var http = require('http');
 var opcoesHTTP = utils.opcoesHTTP("");
@@ -24695,14 +24756,12 @@ var req = http.request(opcoesHTTP, (res) => {
 	res.on('data', function(chunk){
 		console.log("Chunk: " + chunk);
 		let vetor = JSON.parse(chunk);
-		// for(var i = 0; i < vetor.length; i++){
-		// 	console.log("Usuario #" + i + "\n Nome: " + vetor[i].nome);
-
-		// }
-		document.getElementById('nomeUsuaroioLista0').innerHTML = vetor[0].nome;
+		criaElementos(vetor);
 	});
 });
 req.end();
+
+
 },{"./../../utils.js":168,"http":155}],168:[function(require,module,exports){
 (function (Buffer){
 module.exports = {
