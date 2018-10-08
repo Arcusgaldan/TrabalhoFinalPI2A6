a2089ca760ecdf06dd912332cde30bced9d746b2
JulieneCaroline <julienecaroline79@gmail.com>
2018-10-07 22:05:59 -0300

    Correção utils

--- utils.js
+++ utils.js
@@ -25,18 +25,18 @@ module.exports = {
 		var retorno = {
 			hostname: "localhost",
 		    port: 8080,
-		    mode: 'no-cors',
+		    //mode: 'no-cors',
 		    //Access-Control-Allow-Origin: "http://localhost",
 		    method: 'POST',
 		    headers: {
 		      'Content-Type': 'text/plain',    
 		      'Content-Length': Buffer.byteLength(texto),
 		      'Objeto': null,
-		      'Operacao': null,
-		      'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, Objeto, Operacao',
-		      'Access-Control-Allow-Origin': 'http://localhost',
-		      'Access-Control-Allow-Credentials': true,
-		      'Access-Control-Allow-Methods': 'OPTION, GET, POST'
+		      'Operacao': null
+		      // 'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, Objeto, Operacao',
+		      // 'Access-Control-Allow-Origin': 'http://localhost',
+		      // 'Access-Control-Allow-Credentials': true,
+		      // 'Access-Control-Allow-Methods': 'OPTION, GET, POST'
 	    	}
 	    };
 		return retorno;
