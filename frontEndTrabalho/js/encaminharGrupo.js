e42e47327a6d7b01a0935b3a3814ccec4c8f6ada
Gabiruu <gaalvesreis@gmail.com>
2018-11-11 23:57:04 -0200

    Listando itens grupo

--- frontEndTrabalho/js/encaminharGrupo.js
+++ frontEndTrabalho/js/encaminharGrupo.js
@@ -1,3 +1,115 @@
+function buscaGrupo(sigla, cb){
+    var utils = require('./../../utils.js');
+    var http = require('http');
+    var objeto = {
+        campo: "sigla",
+        valor: sigla
+    };
+    var texto = JSON.stringify(objeto);
+
+    var opcoesHTTP = utils.opcoesHTTP(texto);
+    opcoesHTTP.headers.Objeto = "Grupo";
+    opcoesHTTP.headers.Operacao = "BUSCAR";
+
+    var req = http.request(opcoesHTTP, (res) => {
+        console.log("Chegou a resposta!");
+        res.setEncoding('utf8');
+
+        if(res.statusCode == 200){
+            res.on('data', function(chunk){
+                var grupo = JSON.parse(chunk).resultado[0];
+                cb(grupo.id);
+            });
+        }else{
+            cb(0);
+        }
+    });
+
+    req.write(texto);
+    req.end();
+}
+
+function listarEquipamentos(){
+    var url = window.location.pathname;
+    buscaGrupo(url.split("/")[2], function(idGrupo){
+        var utils = require("./../../utils.js");
+        utils.enviaRequisicao("Equipamento", "BUSCAR", {campo: "codGrupo", valor: idGrupo}, function(res){
+            if (res.statusCode == 200){
+                var msg = "";
+                res.on("data", function(chunk){
+                    msg += chunk;
+                });
+                res.on("end", function(){
+                    var vetorEquipamento = JSON.parse(msg).resultado;
+                    for (let i = 0; i < vetorEquipamento.length; i++) {
+                        $("#equipamentosGrupo").append(vetorEquipamento[i].nome + "<br>");
+                    }
+                });
+            }else{
+                document.getElementById('msgErroModal').innerHTML = "Falha ao buscar Equipamento";
+                $("#erroModal").modal('show');
+            }
+        });
+    });
+}
+
+function listarPesquisas(){
+    var url = window.location.pathname;
+    buscaGrupo(url.split("/")[2], function(idGrupo){
+        var utils = require("./../../utils.js");
+        utils.enviaRequisicao("Pesquisa", "BUSCARGRUPO", {idGrupo: idGrupo}, function(res){
+            if (res.statusCode == 200){
+                var msg = "";
+                res.on("data", function(chunk){
+                    msg += chunk;
+                });
+                res.on("end", function(){
+                    var vetorPesquisa = JSON.parse(msg);
+                        for (let i = 0; i < vetorPesquisa.length; i++) {
+                            if (vetorPesquisa[i].dataFim == "1001-01-01"){
+                                $("#pesqAtivosGrupo").append(vetorPesquisa[i].titulo + "<br>");
+                            }else{
+                                $("#pesqInativosGrupo").append(vetorPesquisa[i].titulo + "<br>");
+                        }
+                    }
+                });
+            }else{
+                document.getElementById('msgErroModal').innerHTML = "Falha ao buscar Pesquisa";
+                $("#erroModal").modal('show');
+            }
+        });
+    });
+}
+
+function listarPublicacoes(){
+    var url = window.location.pathname;
+    buscaGrupo(url.split("/")[2], function(idGrupo){
+        var utils = require("./../../utils.js");
+        utils.enviaRequisicao("Publicacao", "BUSCARGRUPO", {idGrupo: idGrupo}, function(res){
+            if (res.statusCode == 200){
+                var msg = "";
+                res.on("data", function(chunk){
+                    msg += chunk;
+                });
+                res.on("end", function(){
+                    console.log("resultado publicacoes: " + vetorPublicacao.length);
+                    var vetorPublicacao = JSON.parse(msg);
+                    for (let i = 0; i < vetorPublicacao.length; i++) {
+                        $("#publicacoesGrupo").append(vetorPublicacao[i].titulo + "<br>");
+                    }
+                });
+            }else{
+                document.getElementById('msgErroModal').innerHTML = "Falha ao buscar Publicacao";
+                $("#erroModal").modal('show');
+            }
+        });
+    });
+}
+
+listarEquipamentos();
+listarPesquisas();
+listarPublicacoes();
+
 var url = window.location.pathname;
 var siglaGrupo = url.split("/")[2];
 if(!siglaGrupo){
@@ -74,9 +186,6 @@ var req = http.request(opcoesHTTP, (res) => {
     	console.log("Erro de conexão ao servidor/banco.");
     }
 }); 
-
-
-
 try{
 	req.write(texto);
 	req.end();
